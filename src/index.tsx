53647682c6d1b6bfa56c64dc80e493ca7281ad59
shoheih <shoheigithub@gmail.com>
2020-05-17 13:11:11 +0900

    add some components

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ThemeProvider } from 'styled-components';
 
+import { Card, Content, Title } from './components';
 import { unregister } from './core';
 import { GlobalStyles, theme } from './styles';
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
     <GlobalStyles />
-    <div>Hello World</div>
+    <Content data-cy='content'>
+      <Title data-cy='title'>Sudoku</Title>
+      <Card data-cy='card'>This is a work in progress</Card>
+    </Content>
   </ThemeProvider>,
   document.getElementById('root')
 );
