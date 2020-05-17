ca1e22b8c1a4810012be2e7cead55062bfe42232
shoheih <shoheigithub@gmail.com>
2020-05-17 16:07:47 +0900

    add GRID TYPES

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ThemeProvider } from 'styled-components';
 
-import { Card, Content, Grid, Title } from './components';
-import { unregister } from './core';
-import { GlobalStyles, theme } from './styles';
+import { Card, Content, Grid, Title } from 'components';
+import { unregister } from 'core';
+import { GlobalStyles, theme } from 'styles';
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
