38188b39f92e936db61492390cd36b1b091d63da
shoheih <shoheigithub@gmail.com>
2020-05-17 21:11:29 +0900

    add isInSquare

---
+++ src/utils/is-in/square/index.ts
@@ -0,0 +1,16 @@
+import { NUMBERS, SQUARE } from '../../../typings';
+
+interface IInput {
+  square: SQUARE;
+  value: NUMBERS;
+}
+
+/**
+ *
+ * @param param0
+ */
+function isInSquare({ square, value }: IInput): boolean {
+  return [...square[0], ...square[1], ...square[2]].includes(value);
+}
+
+export default isInSquare;
