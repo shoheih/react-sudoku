6a963851b5bf9c7d4cedd4c4ea749e1056a0d7e2
shoheih <shoheigithub@gmail.com>
2020-05-21 19:08:46 +0900

    Making The Game

--- src/utils/shuffle/index.test.ts
+++ src/utils/shuffle/index.test.ts
@@ -1,14 +1,15 @@
 import shuffle from './';
+import { NUMBERS } from 'typings';
 
 describe('shuffle', () => {
   it('returns an array with the same length after being shuffled', () => {
-    const array = [1, 2, 3];
+    const array = [1, 2, 3] as NUMBERS[];
     shuffle(array);
     expect(array).toHaveLength(3);
   });
 
   it('returns and arraywith the same elements after being shuffled', () => {
-    const array = [1, 2, 3];
+    const array = [1, 2, 3] as NUMBERS[];
     shuffle(array);
     console.log(array);
     expect(array).toContain(1);
